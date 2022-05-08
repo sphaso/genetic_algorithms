e6e4782f376b9077697d1be43552f7cd8e0a3720
sphaso <sphaso@users.noreply.github.com>
2022-05-08 14:54:11 +0200

    onemax refactor

---
+++ onemax/src/Types.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+module Types where
+
+import System.Random (initStdGen, uniformR, StdGen)
+import Control.Monad.State (State, evalState, get, put, replicateM)
+
+type Petri a = State StdGen a
+type Population a = [a]
+
+data Chromo a = Chromo { genes :: a, age :: Int, c_fitness :: Int }
+
+class Chromosome a where
+    mkChromosome :: Petri a
+    fitness :: a -> Int
+    mutate :: a -> Petri a
+
+class Chromosome a => Generation a where
+    mkPopulation :: Petri (Population a)
+    evaluate :: Population a -> Population a
+    select :: Population a -> [(a, a)]
+    crossover :: [(a, a)] -> Petri (Population a)
+    evolve :: Int -> Population a -> Petri (Population a)
+    evolve maxFitness pop = do
+      let eval = evaluate pop
+      if fitness (head eval) >= maxFitness then
+        return pop
+      else do
+        children <- crossover $ select eval
+        evolve maxFitness =<< mapM mutate children
+
+
